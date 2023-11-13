a855f54aa0f503cf5d68d39524f75ef8121e38a5
Oleksandr Vedmedenko <vedmedenko.alex@gmail.com>
2023-11-14 00:24:18 +0200

    2 tests were added

--- src/test/Skills.test.js
+++ src/test/Skills.test.js
@@ -1,38 +1,38 @@
-// import React from 'react';
-// import { render, screen, fireEvent } from '@testing-library/react';
-// import '@testing-library/jest-dom/extend-expect';
-// import { Provider } from 'react-redux';
-// import configureStore from 'redux-mock-store';
-// import thunk from 'redux-thunk';
-// import Skills from '../components/Expertise/skills';
-// import store from "../store"
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import Skills from '../components/Expertise/skills';
+import store from "../store"
 
 
-// describe('Skills component', () => {
-//   it('renders the Skills component with provided data', () => {
-//     const skillsData = [
-//       { type: 'React', level: 80 },
-//       { type: 'Redux', level: 70 },
-//     ];
+describe('Skills component', () => {
+  it('renders the Skills component with provided data', () => {
+    const skillsData = [
+      { type: 'React', level: 80 },
+      { type: 'Redux', level: 70 },
+    ];
 
-//     render(
-//       <Provider store={store}>
-//         <Skills />
-//       </Provider>
-//     );
+    render(
+      <Provider store={store}>
+        <Skills />
+      </Provider>
+    );
 
-//     expect(screen.getByText('React')).toBeInTheDocument();
-//     expect(screen.getByText('Redux')).toBeInTheDocument();
-//   });
+    expect(screen.getByText('React')).toBeInTheDocument();
+    expect(screen.getByText('Redux')).toBeInTheDocument();
+  });
 
-//   it('renders the Skills component with edit panel visibility toggle', () => {
+  it('renders the Skills component with edit panel visibility toggle', () => {
 
-//     render(
-//       <Provider store={store}>
-//         <Skills />
-//       </Provider>
-//     );
-//     fireEvent.click(screen.getByText('Edit skills'));
-//     expect(screen.getByText('Skill range')).toBeInTheDocument();
-//   });
-// });
+    render(
+      <Provider store={store}>
+        <Skills />
+      </Provider>
+    );
+    fireEvent.click(screen.getByText('Edit skills'));
+    expect(screen.getByText('Skill range')).toBeInTheDocument();
+  });
+});
